2cc08da364e00d5990ffa83e1c36498ced3fd475
mikdevio <mikdevio@gmail.com>
2026-04-15 19:37:22 -0500

    refactor: Separate configuration of express app from server entrypoint

---
+++ src/app.ts
@@ -0,0 +1,33 @@
+
+import express, { type Application } from 'express';
+
+import cors from 'cors';
+
+import apiRouter from "./routes/index.js"
+
+// Iniciando servidor express
+const app: Application = express();
+
+
+// --- Global middlewares ---
+
+// Enable CORS for request from other domains
+app.use(cors());
+
+// Middleware to parse JSON from request body
+app.use(express.json());
+
+// Middleware para parsear datos de formularios (opcional)
+app.use(express.urlencoded({ extended: true }));
+
+// --- Routes definition ---
+
+// Main route of the centralized api and version
+app.use("/api/v1", apiRouter);
+
+// --- Manejo de Errores ---
+
+// Este middleware debe ir SIEMPRE después de las rutas
+// TODO: Create a middleware to deal with all errrors
+
+export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,35 +1,28 @@
 // app.ts
 import 'reflect-metadata';
-import express, { type Request, type Response } from 'express';
 import dotenv from 'dotenv';
-import cors from 'cors';
 
-
-import apiRouter from "./routes/index.js"
-
+import app from './app.js';
 import { AppDataSource } from './config/data_source.js';
+
 
 // Variables de configuración
 dotenv.config();
 
-// Iniciando servidor express
-const app = express();
-
-// Agregando funcionalidades
-app.use(cors());
-app.use(express.json());
-
-// Configurando la base de datos
-AppDataSource.initialize()
-    .then(() => console.log("PSQL connected."))
-    .catch((error) => console.error("Error during connection:", error))
-
 // Obteniendo valor del puerto de conexión
 const PORT = process.env.PORT || 3300;
 
-app.use("/", apiRouter);
+// Configurando la base de datos
+AppDataSource.initialize()
+    .then(() => {
+        console.log("PSQL connected.");
+        // Iniciando servidor en localhost:pruerto
+        app.listen(PORT, () => {
+            console.log(`Server running on http://localhost:${PORT}`);
+        })
+    })
+    .catch((error) => console.error("Error during connection:", error))
 
-// Iniciando servidor en localhost:pruerto
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-})
+
+
+
