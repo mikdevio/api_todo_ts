b93c7f625187c98d0965a30483de1e9d78c2d35b
mikdevio <mikdevio@gmail.com>
2026-04-19 18:56:32 -0500

    fix: repository mock Partial type definition error on test

--- src/test/unit/CategoryService.test.ts
+++ src/test/unit/CategoryService.test.ts
@@ -1,9 +1,6 @@
 import { AppDataSource } from '@/config/data_source.js';
 import { CategoryService } from '../../services/CategoryService.js';
-import {
-  createMockRepository,
-  type MockRepository,
-} from '../mocks/repository.mock.js';
+import { createMockRepository } from '../mocks/repository.mock.js';
 
 jest.mock('../../src/config/data_source', () => ({
   AppDataSource: {
@@ -13,7 +10,7 @@ jest.mock('../../src/config/data_source', () => ({
 
 describe('CategoryServie (unitest with mocks)', () => {
   let categoryService: CategoryService;
-  let mockRepo: MockRepository;
+  let mockRepo: ReturnType<typeof createMockRepository>;
 
   beforeEach(() => {
     mockRepo = createMockRepository();
@@ -23,5 +20,47 @@ describe('CategoryServie (unitest with mocks)', () => {
     categoryService = new CategoryService();
   });
 
-  describe('createCategory', () => {});
+  describe('createCategory', () => {
+    it('debería guardar una categoría y retornar el resultado con fecha', async () => {
+      const dto = { name: 'Ropa', description: 'Moda de verano' };
+      const expectedResult = { id: 1, ...dto, createdAt: new Date() };
+
+      // Programamos el comportamiento (Stub)
+      mockRepo.save.mockResolvedValue(expectedResult);
+
+      const result = await categoryService.createCategory(dto);
+
+      expect(result).toEqual(expectedResult);
+      expect(mockRepo.save).toHaveBeenCalledTimes(1);
+      // Validamos que se instanció correctamente antes de guardar
+      expect(mockRepo.save).toHaveBeenCalledWith(
+        expect.objectContaining({
+          name: 'Ropa',
+        }),
+      );
+    });
+  });
+
+  describe('getCategoryByID', () => {
+    it('debería llamar a findBy con el id correcto', async () => {
+      const mockCategories = [{ id: 5, name: 'Hogar' }];
+      mockRepo.findBy.mockResolvedValue(mockCategories);
+
+      const result = await categoryService.getCategoryByID(5);
+
+      expect(result).toEqual(mockCategories);
+      expect(mockRepo.findBy).toHaveBeenCalledWith({ id: 5 });
+    });
+  });
+
+  describe('deleteCategoryByID', () => {
+    it('debería retornar el resultado del borrado', async () => {
+      mockRepo.delete.mockResolvedValue({ affected: 1 });
+
+      const result = await categoryService.deleteCategoryByID(10);
+
+      expect(result.affected).toBe(1);
+      expect(mockRepo.delete).toHaveBeenCalledWith(10);
+    });
+  });
 });
